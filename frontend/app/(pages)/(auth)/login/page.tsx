c10fe1e7c16cee0c5b473138324e56f5c79eea85
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-13 18:22:16 -0300

    feat(frontent): add providers button component (not connected to the backend)

--- frontend/app/(pages)/(auth)/login/page.tsx
+++ frontend/app/(pages)/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import { AuthProviderButton } from '@/components/button/auth-provider-button';
 import Link from 'next/link';
 import LoginForm from './_components/login';
 
@@ -5,7 +6,13 @@ export default function IniciarSesionPage() {
   return (
     <section className='flex flex-col gap-4 rounded-md bg-secondary p-6 shadow-lg'>
       <h1>Iniciar Sesión</h1>
-      <LoginForm />
+      <section className='flex flex-col gap-4'>
+        <LoginForm />
+        <footer className='flex flex-col gap-1 border-t pt-4'>
+          <AuthProviderButton provider='Google' />
+          <AuthProviderButton provider='Apple' />
+        </footer>
+      </section>
       <footer className='flex justify-center'>
         <p>
           Aún no tenes una cuenta?{' '}
