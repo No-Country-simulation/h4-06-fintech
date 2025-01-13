2fc431165c7ad0db27f800ada09a79b1c901296f
Milo <zapatacamilo86@gmail.com>
2025-01-13 10:42:29 -0500

    Descripción de los cambios realizados

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import {UsersService} from "../../users/users.service";
+
+export type JwtPayload = {
+	id: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+	constructor(private readonly usersService: UsersService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			secretOrKey: process.env.JWT_SECRET,
+			ignoreExpiration: false,
+		});
+	}
+
+	async validate(payload: JwtPayload) {
+		const user = await this.usersService.findOne(payload.id);
+
+		if (!user) {
+			throw new UnauthorizedException();
+		}
+
+		return user;
+	}
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+
+  const config = new DocumentBuilder()
+      .setTitle('Fintech API')
+      .setDescription('The iupi API description')
+      .addBearerAuth()
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
   console.log(`🚀 Server is running on: http://localhost:${port}`);
