c948ae9b2ee4ad4bb862a8d213e1b6e31103b860
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-14 14:34:55 -0300

    style(frontend): center auth elements

--- frontend/app/(pages)/(unauthenticated)/(auth)/layout.tsx
+++ frontend/app/(pages)/(unauthenticated)/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <section className='mx-auto grid min-h-dvh max-w-screen-xl content-center gap-6 p-6 sm:grid-cols-2'>
+    <section className='mx-auto grid max-w-screen-xl content-center gap-6 p-6 py-16 sm:grid-cols-2'>
       <section className='flex flex-col justify-center gap-2'>
         <h2 className='text-xl font-semibold'>
           Forma parte de nuestra comunidad
--- frontend/app/(pages)/(unauthenticated)/layout.tsx
+++ frontend/app/(pages)/(unauthenticated)/layout.tsx
@@ -1,11 +1,15 @@
+import { Footer } from '@/components/common/footer/footer';
 import { Header } from '@/components/common/header/header';
 import { PropsWithChildren } from 'react';
 
 export default function UnauthenticatedLayout({ children }: PropsWithChildren) {
   return (
-    <section className='relative pt-[72px]'>
+    <section className='relative'>
       <Header />
-      {children}
+      <section className='grid min-h-dvh grid-rows-[1fr_auto] pt-[72px]'>
+        {children}
+        <Footer />
+      </section>
     </section>
   );
 }
