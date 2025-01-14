5d66b7b2fd5b398812c982fb20891148f73b1585
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-14 14:34:16 -0300

    feat(frontend): add footer common component

--- frontend/app/components/common/header/header.tsx
+++ frontend/app/components/common/header/header.tsx
@@ -4,19 +4,21 @@ import Link from 'next/link';
 
 export function Header() {
   return (
-    <header className='fixed top-0 w-full border-b border-border'>
+    <header className='fixed top-0 z-50 w-full border-b border-border bg-background'>
       <div className='mx-auto flex max-w-screen-xl items-center justify-between px-6 py-4'>
-        <section className='flex items-center gap-2'>
-          <div className='relative size-10 rounded-lg bg-blue-600'>
-            <Image
-              src='/images/iupi-logo.png'
-              alt='iupi logo'
-              className='h-full w-full'
-              fill
-            />
-          </div>
-          <h3 className='text-xs uppercase'>Ahorro & inversiones</h3>
-        </section>
+        <Link href='/'>
+          <section className='flex items-center gap-2'>
+            <div className='relative size-10 rounded-lg bg-blue-600'>
+              <Image
+                src='/images/iupi-logo.png'
+                alt='iupi logo'
+                className='h-full w-full'
+                fill
+              />
+            </div>
+            <h3 className='text-xs uppercase'>Ahorro & inversiones</h3>
+          </section>
+        </Link>
         <nav className='flex gap-2'>
           <Link href={'/login'}>
             <Button>Iniciar sesión</Button>
