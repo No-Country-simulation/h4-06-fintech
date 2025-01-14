ce71bde45b1ac466b7dc6dd769ae9a8a85ec178c
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-14 17:11:37 -0300

    ci: set pnpm package manager

--- frontend/app/components/common/footer/footer.tsx
+++ frontend/app/components/common/footer/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className='border-t'>
-      <div className='max-w-screen-xl py-2'>
+    <footer className='w-full border-t'>
+      <div className='mx-auto max-w-screen-xl py-2'>
         <p className='text-center text-sm'>
           H4 - 06 fintech - {new Date().getFullYear()}
         </p>
