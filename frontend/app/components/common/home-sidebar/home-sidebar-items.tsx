9b2699863edbcb98b32c7f330077f2455e5df443
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-13 21:59:49 -0300

    feat(frontend): add basic sidebar component

---
+++ frontend/app/components/common/home-sidebar/home-sidebar-items.tsx
@@ -0,0 +1,29 @@
+import { Home } from 'lucide-react';
+
+export const items = [
+  {
+    title: 'Inicio',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Wallet',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Portfolio',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Gestion de inversiones',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Configuracion',
+    url: '#',
+    icon: Home,
+  },
+];
---
+++ frontend/app/components/common/home-sidebar/home-sidebar.tsx
@@ -0,0 +1,41 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { items } from './home-sidebar-items';
+
+export function HomeSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>Username</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>Cerrar sesión</SidebarFooter>
+    </Sidebar>
+  );
+}
