d8623fb5de969836b94ab56f2054c7f7a5873468
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-14 22:13:59 -0300

    fix(frontend): redirect to google or apple login page when clicking on either provider button

--- frontend/app/components/button/auth-provider-button.tsx
+++ frontend/app/components/button/auth-provider-button.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import envs from '../../../config/envs';
 import { Button } from '../ui/button';
 
 interface Props {
@@ -9,15 +10,19 @@ export function AuthProviderButton({ provider }: Props) {
   const iconPath =
     provider === 'Google' ? '/svg/google-icon.svg' : '/svg/apple-icon.svg';
 
+  const redirectLink = provider === 'Google' ? '/google' : '/apple';
+
   return (
-    <Button className='relative bg-background text-foreground hover:bg-input'>
-      <Image
-        src={iconPath}
-        alt={`${provider} icon`}
-        width={20}
-        height={20}
-      />
-      {provider}
-    </Button>
+    <a href={envs.BACKEND_URL + redirectLink}>
+      <Button className='relative w-full bg-background text-foreground hover:bg-input'>
+        <Image
+          src={iconPath}
+          alt={`${provider} icon`}
+          width={20}
+          height={20}
+        />
+        {provider}
+      </Button>
+    </a>
   );
 }
