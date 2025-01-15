40f89f77c844b94f642c2e7daaba4a866d51b20a
Everjr18 <91352297+Everjr18@users.noreply.github.com>
2025-01-15 16:40:08 -0500

    chore(frontend):  fix a problem in build enviroment

--- frontend/app/(pages)/(authenticated)/onboarding/[slug]/page.tsx
+++ frontend/app/(pages)/(authenticated)/onboarding/[slug]/page.tsx
@@ -1,11 +1,28 @@
-import ProfileOverview from "./_components/profile-overview";
+import ProfileOverview from './_components/profile-overview';
 
-export default async function OnboardingPersonalizationPage({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
+// export default async function OnboardingPersonalizationPage({
+//   params,
+// }: {
+//   params: Promise<{ slug: string }>
+// }) { {
+//   const slug = (await params).slug;
+
+//   return {
+
+//     <div className="container mx-auto py-8">
+//       <ProfileOverview />
+//     </div>
+//   }
+// }
+export default async function OnboardingPersonalizationPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
   console.log(slug);
-
   return (
-    <div className="container mx-auto py-8">
+    <div className='container mx-auto py-8'>
       <ProfileOverview />
     </div>
   );
