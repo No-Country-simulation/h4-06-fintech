3e079d6b08e715eea9cf86f647b19a3fbe82dc62
Everjr18 <91352297+Everjr18@users.noreply.github.com>
2025-01-15 16:21:34 -0500

    chore(frontend) connect database

--- frontend/app/(pages)/(authenticated)/onboarding/page.tsx
+++ frontend/app/(pages)/(authenticated)/onboarding/page.tsx
@@ -1,6 +1,7 @@
 import OnboardingForm from "./_components/onboardingform";
 
 export default function OnboardingPage() {
+  
   return (
     <div className="max-w-2xl mx-auto  p-4">
       <h1 className="text-2xl font-bold text-center ">Hola usuario, bienvenido a <span className="text-blue-600">IUPIA</span></h1>
