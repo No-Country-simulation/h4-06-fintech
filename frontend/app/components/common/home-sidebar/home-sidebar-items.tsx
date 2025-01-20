922e1df0b916fa865871914d68fd0657b61be2c9
Everjr18 <91352297+Everjr18@users.noreply.github.com>
2025-01-20 12:14:09 -0500

    feat(frontend): add setting page

--- frontend/app/components/common/home-sidebar/home-sidebar-items.tsx
+++ frontend/app/components/common/home-sidebar/home-sidebar-items.tsx
@@ -1,35 +1,35 @@
 import {
-  ChartAreaIcon,
-  GanttChartIcon,
-  Home,
-  SettingsIcon,
-  Wallet2Icon,
-} from 'lucide-react';
+	ChartAreaIcon,
+	GanttChartIcon,
+	Home,
+	SettingsIcon,
+	Wallet2Icon,
+} from "lucide-react";
 
 export const items = [
-  {
-    title: 'Inicio',
-    url: '#',
-    icon: Home,
-  },
-  {
-    title: 'Wallet',
-    url: '#',
-    icon: Wallet2Icon,
-  },
-  {
-    title: 'Portfolio',
-    url: '#',
-    icon: GanttChartIcon,
-  },
-  {
-    title: 'Gestion de inversiones',
-    url: '#',
-    icon: ChartAreaIcon,
-  },
-  {
-    title: 'Configuracion',
-    url: '#',
-    icon: SettingsIcon,
-  },
+	{
+		title: "Inicio",
+		url: "#",
+		icon: Home,
+	},
+	{
+		title: "Wallet",
+		url: "#",
+		icon: Wallet2Icon,
+	},
+	{
+		title: "Portfolio",
+		url: "#",
+		icon: GanttChartIcon,
+	},
+	{
+		title: "Gestion de inversiones",
+		url: "#",
+		icon: ChartAreaIcon,
+	},
+	{
+		title: "Configuracion",
+		url: "/setting",
+		icon: SettingsIcon,
+	},
 ];
