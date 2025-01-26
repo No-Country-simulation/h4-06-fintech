6653e3c8eb49b5ef45707bdbafa67896bf5ecd83
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-26 14:09:38 -0300

    fix(backend): add missing class validator property

--- backend/src/target/dto/create-target.dto.ts
+++ backend/src/target/dto/create-target.dto.ts
@@ -1,13 +1,15 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsPositive, IsString } from 'class-validator';
 
 export class CreateTargetDto {
   @IsString()
   name: string;
 
   @IsNumber()
+  @IsPositive()
   amount: number;
 
   @IsNumber()
+  @IsPositive()
   durationMonths: number;
 
   @IsString()
