fec69b4872788bdbb5fcc35339674e1e5b8033a0
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-27 14:41:42 -0300

    feat(frontend): add text when there are no financial target to show

--- frontend/app/(pages)/(authenticated)/(tabs)/financial-target/page.tsx
+++ frontend/app/(pages)/(authenticated)/(tabs)/financial-target/page.tsx
@@ -1,16 +1,17 @@
 import { Text } from '@/components/ui/text';
+import { backend } from '@api';
+import { TargetsEmpty } from './_components/targets-empty';
 import { TargetsList } from './_components/targets-list';
 
-export default function page() {
+export default async function page() {
+  const { target: targets } = await backend.authApi.getProfile();
+
   return (
     <section className='flex flex-col gap-12'>
       <header className='flex flex-col gap-4'>
-        <Text variant='header'>Objetivos financieros</Text>
-        <Text variant='small'>¿Cómo lo vas logrando?</Text>
+        <Text variant='header'>Mis objetivos</Text>
       </header>
-      <section>
-        <TargetsList />
-      </section>
+      {targets.length ? <TargetsList targets={targets} /> : <TargetsEmpty />}
     </section>
   );
 }
