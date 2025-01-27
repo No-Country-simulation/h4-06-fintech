40e5a2524b0062bda3de0420919eab1d9443a2b6
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-27 13:57:19 -0300

    feat(frontend): add header with navbar

--- frontend/app/(pages)/(authenticated)/(dashboard)/setting/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NotificationPermission } from "./_components/notification-permission";
-
-export default function SettingPage() {
-	return (
-		<div>
-			<NotificationPermission />
-		</div>
-	);
-}
