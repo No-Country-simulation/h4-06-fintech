0ee0882dae70fc87ceed01dba87296cea4d61684
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-29 11:54:14 -0300

    feat(frontend): add stock page

---
+++ frontend/app/(pages)/(authenticated)/(tabs)/investment/stock/_components/ticker-item.tsx
@@ -0,0 +1,57 @@
+import { Text } from '@/components/ui/text';
+import { formatMoney } from '@/lib/money-formatter';
+import { Ticker } from 'client-api/backend/modules/investment/stock/interface/getTickers';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props {
+  ticker: Ticker;
+}
+
+export function TickerItem({ ticker }: Props) {
+  const changeAmount = (
+    ticker.price.current - ticker.price.previousClose
+  ).toFixed(2);
+
+  const changePercent = (
+    (Number(changeAmount) / ticker.price.previousClose) *
+    100
+  ).toFixed(2);
+
+  const isUp = ticker.price.current > ticker.price.previousClose;
+
+  return (
+    <Link
+      className='flex items-center justify-between p-2 transition-colors hover:bg-primary/10'
+      key={ticker.symbol}
+      href={`/investment/stock/${ticker.symbol}`}
+    >
+      <section className='flex items-center gap-2'>
+        <Text>{ticker.companyName}</Text>
+        <Image
+          alt='stock evolution'
+          src={isUp ? '/svg/is-up.svg' : '/svg/is-down.svg'}
+          height={24}
+          width={24}
+        />
+      </section>
+      <section className='flex items-center gap-9'>
+        <div className='rounded-lg border border-foreground px-4 py-2'>
+          <p className='text-xl font-medium'>
+            {formatMoney(ticker.price.current)}
+          </p>
+        </div>
+        <div
+          data-isup={isUp}
+          className='flex items-center gap-2 rounded-lg border border-foreground border-red-600 bg-red-300 px-4 py-2 text-red-600 data-[isup=true]:border-green-700 data-[isup=true]:bg-green-300 data-[isup=true]:text-green-700'
+        >
+          <p className='text-xl font-medium'>
+            {isUp && '+'}
+            {formatMoney(changeAmount)}
+          </p>
+          <p className='text-xl font-medium'>({changePercent})%</p>
+        </div>
+      </section>
+    </Link>
+  );
+}
--- frontend/client-api/backend/modules/investment/stock/stock.api.ts
+++ frontend/client-api/backend/modules/investment/stock/stock.api.ts
@@ -22,22 +22,42 @@ const stockApi: StockApi = {
       {
         companyName: 'Microsoft Corporation Common Stock',
         symbol: 'MSFT',
+        price: {
+          current: 234.2,
+          previousClose: 220.2,
+        },
       },
       {
         companyName: 'Apple Inc. Common Stock',
         symbol: 'AAPL',
+        price: {
+          current: 234.4,
+          previousClose: 250.2,
+        },
       },
       {
         companyName: 'NVIDIA Corporation Common Stock',
         symbol: 'NVDA',
+        price: {
+          current: 234.4,
+          previousClose: 281.2,
+        },
       },
       {
         companyName: 'Amazon.com, Inc. Common Stock',
         symbol: 'AMZN',
+        price: {
+          current: 234.4,
+          previousClose: 232.2,
+        },
       },
       {
         companyName: 'Alphabet Inc. Class C Capital Stock',
         symbol: 'GOOG',
+        price: {
+          current: 234.4,
+          previousClose: 209.2,
+        },
       },
     ];
   },
