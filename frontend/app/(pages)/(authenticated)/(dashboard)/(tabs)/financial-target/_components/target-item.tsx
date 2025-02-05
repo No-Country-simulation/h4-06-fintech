7a49b46dd4d237a5b49b666750f653d21f373984
Santiago Zarate <santiago2001zarate@gmail.com>
2025-02-05 13:22:19 -0300

    style(frontend): update financial target item

--- frontend/app/(pages)/(authenticated)/(dashboard)/(tabs)/financial-target/_components/target-item.tsx
+++ frontend/app/(pages)/(authenticated)/(dashboard)/(tabs)/financial-target/_components/target-item.tsx
@@ -18,19 +18,11 @@ export function TargetItem({ target }: Props) {
       key={target.id}
       className='group'
     >
-      <li className='flex w-full items-center justify-between rounded-md border border-foreground bg-background p-2 transition-colors group-hover:bg-primary/10'>
+      <li className='flex w-full items-center justify-between rounded-md bg-background p-2 shadow-md shadow-foreground/30 transition-colors group-hover:bg-primary/10'>
         <section className='flex items-center gap-2'>
-          <div className='flex aspect-square size-11 items-center justify-center rounded-full bg-accent/50 p-2 text-xs'>
-            {percentaje}
-          </div>
+          <p className='px-2 text-2xl font-bold text-primary'>{percentaje}</p>
           <p>{target.name}</p>
         </section>
-        {/* <div
-          data-active={target.isActive}
-          className='rounded-full bg-red-200 px-4 py-1 text-xs text-red-600 data-[active=true]:bg-green-200 data-[active=true]:text-green-600'
-        >
-          {target.isActive ? 'Activo' : 'Inactivo'}
-        </div> */}
       </li>
     </Link>
   );
