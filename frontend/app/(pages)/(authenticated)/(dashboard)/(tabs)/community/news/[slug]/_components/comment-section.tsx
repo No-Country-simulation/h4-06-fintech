9ead9d14afdbf9e543d20ad2552fc60e047b0960
Santiago Zarate <santiago2001zarate@gmail.com>
2025-02-06 16:18:06 -0300

    fix(frontend): show news and comments

--- frontend/app/(pages)/(authenticated)/(dashboard)/(tabs)/community/news/[slug]/_components/comment-section.tsx
+++ frontend/app/(pages)/(authenticated)/(dashboard)/(tabs)/community/news/[slug]/_components/comment-section.tsx
@@ -1,37 +1,48 @@
-import { getDifferenceDays } from "@/lib/get-difference-day";
-import { CommentForm } from "./comment-form";
-import Image from "next/image";
-import { Comment } from "client-api/backend/modules/community/news/interface/getOne";
-
+import { getDifferenceDays } from '@/lib/get-difference-day';
+import { CommentForm } from './comment-form';
+import Image from 'next/image';
+import { Comment } from 'client-api/backend/modules/community/news/interface/getOne';
+import { fakeComments } from '../_data/comments';
 
 interface Props {
-  comments: Comment[]
+  comments: Comment[];
 }
 
 export function CommentSection({ comments }: Props) {
+  const newComments = [...fakeComments, ...comments];
+
   return (
-    <section className="flex flex-col gap-8">
-      <header className="flex flex-col">
+    <section className='flex flex-col gap-8'>
+      <header className='flex flex-col'>
         <p>{comments.length} comentarios</p>
       </header>
       <CommentForm />
-      <ul className="flex flex-col gap-8">
-        {comments.map((comment, index) => (
-          <li key={index} className="flex gap-2 items-center">
-            <section className="">
-              <div className="p-2 rounded-full border border-border bg-primary/50">
-                <Image src="/svg/user.svg" alt="user icon" width={24} height={24} className="" />
+      <ul className='flex flex-col gap-8'>
+        {newComments.map((comment, index) => (
+          <li
+            key={index}
+            className='flex items-center gap-2'
+          >
+            <section className=''>
+              <div className='rounded-full border border-border bg-primary/50 p-2'>
+                <Image
+                  src='/svg/user.svg'
+                  alt='user icon'
+                  width={24}
+                  height={24}
+                  className=''
+                />
               </div>
             </section>
-            <section className="flex flex-col gap-1">
-              <p className="text-xs">{getDifferenceDays(comment.dateComment)}</p>
-              <p className="text-sm">
-                {comment.content}
+            <section className='flex flex-col gap-1'>
+              <p className='text-xs'>
+                {getDifferenceDays(comment.dateComment)}
               </p>
+              <p className='text-sm'>{comment.content}</p>
             </section>
           </li>
         ))}
       </ul>
     </section>
-  )
+  );
 }
--- frontend/client-api/backend/modules/community/news/news.api.ts
+++ frontend/client-api/backend/modules/community/news/news.api.ts
@@ -1,28 +1,39 @@
-import { NewsApi } from "./interface/api.interface";
-import envs from "config/envs";
-import { handleRequest } from "shared/handleRequest";
-import { authRequest } from "shared/authRequest";
-import { AuthError } from "shared/errors";
+import { NewsApi } from './interface/api.interface';
+import envs from 'config/envs';
+import { handleRequest } from 'shared/handleRequest';
+import { authRequest } from 'shared/authRequest';
+import { AuthError } from 'shared/errors';
+import { newsData } from './data/news';
 
-const BASE_URL = `${envs.BACKEND_URL}/news`
+const BASE_URL = `${envs.BACKEND_URL}/news`;
 
 const newsApi: NewsApi = {
   async getAll() {
-    const url = BASE_URL
+    const url = BASE_URL;
+
+    if (process.env.NODE_ENV === 'development') {
+      return newsData;
+    }
 
     return handleRequest({
       fetcherFn: () => authRequest(url),
-      ErrorClass: AuthError
-    })
+      ErrorClass: AuthError,
+    });
+
+    // return newsData;
   },
   async getOne({ id }) {
-    const url = `${BASE_URL}/${id}`
+    if (process.env.NODE_ENV === 'development') {
+      return newsData.find((n) => n.id === Number(id))!;
+    }
+
+    const url = `${BASE_URL}/${id}`;
 
     return handleRequest({
       fetcherFn: () => authRequest(url),
-      ErrorClass: AuthError
-    })
-  }
-}
+      ErrorClass: AuthError,
+    });
+  },
+};
 
-export { newsApi }
+export { newsApi };
